6febbfbd092d5b8f75a17f88c2ad214260e54768
Andrey <ankole@bk.ru>
2024-05-22 21:38:56 +0300

    Подключил bootstrap

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { resolve } from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/github-static-site/',
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap')
+    }
+  }
 })
